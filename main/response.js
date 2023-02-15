8e8c1d7a91d07a4cb10dba2d95f58f556a12ced2
Mudunuri bhaskara karthikeya varma <102793643+karthikmudunuri@users.noreply.github.com>
2023-02-15 17:31:45 +0000

    Add files via upload

---
+++ main/response.js
@@ -0,0 +1,10 @@
+const responseObj = {
+    hi:"Hello,this is from venus verse",
+    hello: "Hey ! How are you doing ?",
+    hey: "Hey! What's Up",
+    products:"please go through this link http://127.0.0.1:5500/index.html",
+    venusverse:"Hello from VenusVentures Inc™ . We are an innovation ecosystem,  nurturing ideas to solve big societal problems using Web3 and emerging technology. Our core objective is to inspire a balanced world by engaging, empowering and enabling a global community to thrive within web3 and emerging technology.",
+
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+  };
